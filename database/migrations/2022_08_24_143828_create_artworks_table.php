530e3dd5aa0db008b0f282d29961eb4ed75ea274
Adam Lopez <adamf04@gmail.com>
2022-08-24 10:42:32 -0400

    artwork controller, model, and database table creation

---
+++ database/migrations/2022_08_24_143828_create_artworks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('artwork_name');
+            $table->string('artwork_path');
+            $table->integer('width');
+            $table->integer('height');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artworks');
+    }
+};
